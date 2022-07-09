aa4a5d3414820a8d36f1d07d7af25a17a8a2432f
onomeh <onwuanibeonome@gmail.com>
2022-07-09 16:39:03 +0000

    Update schema

--- schema.sql
+++ schema.sql
@@ -11,3 +11,21 @@ weight_kg decimal
 
 ALTER TABLE animals
 ADD COLUMN species text;
+
+
+-- Remove column species
+ALTER TABLE animals DROP COLUMN species
+-- species field column
+ALTER TABLE animals ADD species CHAR(250);
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD species_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_species_table FOREIGN KEY(species_id) REFERENCES species(id);
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD owner_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner_table FOREIGN KEY(owner_id) REFERENCES owners(id);
